f771ad0785b0ade0899d6b3263e9cd08b1f0e380
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-16 10:23:41 +0100

    Bootstrap docker needs to see the real /dev
    Otherwise, ipmi.pp cannot usefully wait for /dev/ipmi0

--- install.sh
+++ install.sh
@@ -120,6 +120,7 @@ PUPPETCONF
     docker run --name puppet-bootstrap.service \
            --net=host --privileged \
            -v /mnt:/opt/root \
+           -v /dev:/opt/root/dev \
            -v /mnt/etc/systemd:/etc/systemd \
            -v /mnt/etc/puppet:/etc/puppet \
            -v /mnt/var/lib/puppet:/var/lib/puppet \

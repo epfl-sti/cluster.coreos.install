a6cf01f580b9312e178e94b54369c3989ad7c61f
Dominique Quatravaux <dominique@quatravaux.org>
2016-11-02 21:06:55 +0100

    Correctly parse FOO=http://bar?a=b type vars

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@
 set -e -x
 
 if [ -f "/etc/bootstrap-environment" ]; then
-    eval "$(sed "s/\(.*\)=\(.*\)/: \${\1:='\2'}/g" < /etc/bootstrap-environment)"
+    eval "$(sed "s/\([^=]*\)=\(.*\)/: \${\1:='\2'}/g" < /etc/bootstrap-environment)"
 fi
 
 : ${COREOS_FQDN:=$(hostname -f)}

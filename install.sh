c55341dc113de2fb1644a95bc3adaf1e9f6ce150
domq <dominique@quatravaux.org>
2017-01-30 21:14:22 +0100

    Roll back the previous two commits
    Really, there is no point in restarting networkd during bootstrap.

--- install.sh
+++ install.sh
@@ -136,7 +136,6 @@ PUPPETCONF
            -v /mnt/etc/systemd:/etc/systemd \
            -v /mnt/etc/puppet:/etc/puppet \
            -v /mnt/var/lib/puppet:/var/lib/puppet \
-           -v /run/systemd:/run/systemd \
            -v /home/core:/home/core \
            -v /mnt/usr/bin/systemctl:/usr/bin/systemctl:ro \
            -v /mnt/lib64:/lib64:ro \
